87749f213060131a9dd3c1870dfd8e79c324e65e
shipingan <740425014@qq.com>
2019-10-17 15:12:59 +0800

    feat:巴适的板

--- src/components/productshow/diapers.vue
+++ src/components/productshow/diapers.vue
@@ -4,12 +4,13 @@
     <el-table :data="tableData" style="width: 100%" size="mini" border>
       <el-table-column prop="name" label="产品名称" align="center"></el-table-column>
       <el-table-column prop="number" label="产品数量" align="center"></el-table-column>
-      <el-table-column prop="models" label="产品规格" align="center"></el-table-column>
+      <el-table-column prop="moduls" label="产品规格" align="center"></el-table-column>
       <el-table-column prop="weixin" label="商家微信" align="center"></el-table-column>
       <el-table-column prop="phone" label="商家电话" align="center"></el-table-column>
       <el-table-column label="操作" align="center">
           <template slot-scope="scope">
               <el-button @click="gotoquery(scope.row)" size="mini" type="primary">查看详情</el-button>
+              <el-button @click="gotoquery(scope.row)" size="mini" type="danger">删除</el-button>
           </template>
       </el-table-column>
     </el-table>
@@ -19,33 +20,11 @@
 export default {
   data() {
     return {
-        tableData:[
-            {
-                productID:1,
-                name:"花王",
-                number:'130',
-                models:"nb,s,m,l,xl,xxl",
-                weixin:'13698374541',
-                phone:'13698374541'
-            },
-            {
-                productID:2,
-                name:"宜婴",
-                number:'233',
-                models:"nb,s,m,l,xl,xxl",
-                weixin:'13698374541',
-                phone:'13698374541'
-            },
-            {
-                productID:3,
-                name:"大王",
-                number:'156',
-                models:"nb,s,m,l,xl,xxl",
-                weixin:'13698374541',
-                phone:'13698374541'
-            }
-        ],
+        tableData:[],
     };
+  },
+  mounted(){
+    this.getdiaperdetali()
   },
   methods: {
       gotoquery(row){
@@ -54,6 +33,14 @@ export default {
             productID:row.productID
           }
           this.$router.push({path:url,query:params})
+      },
+      getdiaperdetali(){
+        let url = 'http://localhost:3000/getdiaperdetalis'
+        let params = {}
+        this.$axios.post(url,params).then(res=>{
+          console.log(res);
+          this.tableData = res.data
+        })
       }
   }
 };
